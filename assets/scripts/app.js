b8b43c2fe117acc5248e90a66b3a896ccc9b7a22
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-02 16:47:32 +0000

    MOre on strings

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 const defaultResult = 0;
 let currentResult = defaultResult; // can be changed, notice the camel case notation, cannot start with a digit
-currentResult += "(100 * 3) / 2 - 1";
-let calculatorDEcription = "(" + currentResult+")";
-outputResult(currentResult, calculatorDEcription);
+currentResult += "'(100 * 3)' / `2 - 1`";
+let calculatorDecription = "(" + currentResult+")";
+outputResult(currentResult, calculatorDecription);
