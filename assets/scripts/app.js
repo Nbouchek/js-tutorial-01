dda21bb51c9c063779ba9a9c8c7068b49d50c523
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-02 16:43:43 +0000

    more on variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
-let currentResult = 0; // can be changed, notice the camel case notation, cannot start with a digit
-
-currentResult += 100;
-
-outputResult(currentResult, "");
+const defaultResult = 0;
+let currentResult = defaultResult; // can be changed, notice the camel case notation, cannot start with a digit
+currentResult += "(100 * 3) / 2 - 1";
+let calculatorDEcription = "(" + currentResult+")";
+outputResult(currentResult, calculatorDEcription);
