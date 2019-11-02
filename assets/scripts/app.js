c01722a9c845356d104999db22ffbaa6340e7813
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-02 16:17:28 +0000

    introducing mathematical operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,5 @@
-alert("this works!");
+let currentResult = 0; // can be changed, notice the camel case notation, cannot start with a digit
+
+currentResult += 100;
+
+outputResult(currentResult, "");
