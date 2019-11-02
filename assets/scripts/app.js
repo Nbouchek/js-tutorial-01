5b32d66b9a2c6dc184c59de58a01696756eb6321
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-02 17:06:33 +0000

    Demoing features of the tempplat eliteral `${<some variable>}`

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult; // can be changed, notice the camel case notation, cannot start with a digit
-currentResult += `${currentResult} = (100 * 3) / 2 - 1`;
+currentResult += `${currentResult} = (100 * 3) 
+
+/ 2 - 1
+
+` // termplate literal;
 let calculatorDecription = "(" + currentResult + ")";
 outputResult(currentResult, calculatorDecription);
