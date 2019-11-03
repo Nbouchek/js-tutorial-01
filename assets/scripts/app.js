87f9c442a927f98126188318dbe206b1acdcba08
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-03 08:54:08 +0000

    introducing parseInt( ) - parseFloat( )

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult; // can be changed, notice the camel case notation, cannot start with a digit
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, '');
 }
 
-currentResult = add(1, 3);
-
-let calculatorDecription = `${currentResult} = (100 * 3) / 2 - 1`;
-let errorMessage = "'An errors' " + "occurred";
-
-outputResult(currentResult, calculatorDecription);
+addBtn.addEventListener("click", add);
