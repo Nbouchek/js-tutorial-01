dc6b2f97cdec463a614d98620274834fd28cece7
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-03 08:56:13 +0000

    SHorthand of parseInt()/parseFloat() ---> + +var

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@ const defaultResult = 0;
 let currentResult = defaultResult; // can be changed, notice the camel case notation, cannot start with a digit
 
 function add() {
-  currentResult = currentResult + parseInt(userInput.value);
+  currentResult = currentResult + +userInput.value;
   outputResult(currentResult, '');
 }
 
