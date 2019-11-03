533d6017c31eb4a296f4ea313170cde5d9455137
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-03 09:14:46 +0000

    Refactor code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,20 @@
 const defaultResult = 0;
+
 let currentResult = defaultResult; // can be changed, notice the camel case notation, cannot start with a digit
 
+function getUserInput() {
+  const enteredNumber = parseInt(userInput.value);
+  return enteredNumber;
+}
+
+function getDescription(enteredNumber) {
+  return `${currentResult} + ${enteredNumber}`;
+}
+
 function add() {
-  const calcDescription = `${currentResult} + ${userInput.value}`;
-  currentResult = currentResult + parseInt(userInput.value);
+  const enteredNumber = getUserInput();
+  const calcDescription = getDescription(enteredNumber);
+  currentResult = currentResult + enteredNumber;
   outputResult(currentResult, calcDescription);
 }
 
