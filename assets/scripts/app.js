e6e5e0bb91be2d8402e06719551e544f402ea1f6
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-04 14:08:35 +0000

    Introducing the switch statement

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,73 +3,81 @@ let currentResult = defaultResult;
 let logEntries = [];
 
 function getUserInput() {
-  const enteredNumber = parseInt(userInput.value);
-  return enteredNumber;
+    const enteredNumber = parseInt(userInput.value);
+    return enteredNumber;
 }
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calcDescription);
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
 }
 
 function writeToLog(
-  operationIdentifier,
-  prevResult,
-  operationNumber,
-  newResult
+    operationIdentifier,
+    prevResult,
+    operationNumber,
+    newResult
 ) {
-  const logEntry = {
-    operation: operationIdentifier,
-    prevResult: prevResult,
-    number: operationNumber,
-    result: newResult
-  };
-  logEntries.push(logEntry);
-  console.log(logEntry.number);
-  console.log(logEntries);
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntry.number);
+    console.log(logEntries);
 }
 
 function getOperands() {
-  const initialResult = currentResult;
-  const enteredNumber = getUserInput();
-  return { enteredNumber, initialResult };
+    const initialResult = currentResult;
+    const enteredNumber = getUserInput();
+    return {enteredNumber, initialResult};
 }
 
 function calculateResult(clculationType) {
-  const { enteredNumber, initialResult } = getOperands();
-  let mathOperator;
-  if (clculationType === "ADD") {
-    currentResult += enteredNumber;
-    mathOperator = "+";
-  } else if (clculationType === "SUBTRACT") {
-    currentResult -= enteredNumber;
-    mathOperator = "-";
-  } else if (clculationType === "MULTIPLY") {
-    currentResult *= enteredNumber;
-    mathOperator = "x";
-  } else if (clculationType === "DIVIDE") {
-    currentResult /= enteredNumber;
-    mathOperator = "/";
-  }
-  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
-  writeToLog(clculationType, initialResult, enteredNumber, currentResult);
+    const {enteredNumber, initialResult} = getOperands();
+    let mathOperator;
+
+    switch (clculationType) {
+        case "ADD":
+            currentResult += enteredNumber;
+            mathOperator = "+";
+            break;
+        case "SUBTRACT":
+            currentResult -= enteredNumber;
+            mathOperator = "-";
+            break;
+        case "MULTIPLY":
+            currentResult *= enteredNumber;
+            mathOperator = "x";
+            break;
+        case "DIVIDE":
+            currentResult /= enteredNumber;
+            mathOperator = "/";
+            break;
+        default:
+            break;
+    }
+    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+    writeToLog(clculationType, initialResult, enteredNumber, currentResult);
 }
 
 // define functions
 function add() {
-  calculateResult("ADD");
+    calculateResult("ADD");
 }
 
 function subtract() {
-  calculateResult("SUBTRACT");
+    calculateResult("SUBTRACT");
 }
 
 function multiply() {
-  calculateResult("MULTIPLY");
+    calculateResult("MULTIPLY");
 }
 
 function divide() {
-  calculateResult("DIVIDE");
+    calculateResult("DIVIDE");
 }
 
 // add event listeners
